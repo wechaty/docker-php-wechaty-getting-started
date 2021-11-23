4eb193424b1356e4e2c9ff1fff0c75e2801981c0
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-11-24 01:11:37 +0800

    add Dockerfile

--- quick_start.sh
+++ quick_start.sh
@@ -20,7 +20,7 @@ docker run -tid \
   -e WECHATY_TOKEN \
   wechaty/wechaty
 
-sleep 5s
+sleep 10s
 
 export WECHATY_PUPPET_HOSTIE_TOKEN=$WECHATY_TOKEN
 echo "WECHATY_PUPPET_HOSTIE_TOKEN=$WECHATY_PUPPET_HOSTIE_TOKEN"
