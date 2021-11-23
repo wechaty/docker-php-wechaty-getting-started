aa1b347357a55c7e620d645412e0cd8a787bf800
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-11-24 00:26:57 +0800

    add docker php-wechaty start

---
+++ bin/debug.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+export WECHATY_PUPPET_HOSTIE_TOKEN=73626c45-5f3d-4185-b760-8c8b53ad9db6
+echo "WECHATY_PUPPET_HOSTIE_TOKEN=$WECHATY_PUPPET_HOSTIE_TOKEN"
+export WECHATY_PUPPET_HOSTIE_ENDPOINT="localhost:8788"
+
+cd ..
+
+docker run -tid \
+  --rm \
+  --name php-wechaty \
+  -e WECHATY_LOG \
+  -e WECHATY_PUPPET_HOSTIE_TOKEN \
+  -e WECHATY_PUPPET_HOSTIE_ENDPOINT \
+  --volume="$(pwd)":/bot
+  phpwechaty/php-wechaty:v1
+  examples/ding-dong-bot.php
